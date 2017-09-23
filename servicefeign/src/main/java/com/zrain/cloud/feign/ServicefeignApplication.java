0f439030b6f929093b3347f85031fa16dfed098e
Zrain <yumike18@126.com>
2017-09-23 11:32:44 +0800

    1. 给ribbon和feign添加Hystrix仪表盘

--- servicefeign/src/main/java/com/zrain/cloud/feign/ServicefeignApplication.java
+++ servicefeign/src/main/java/com/zrain/cloud/feign/ServicefeignApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ServicefeignApplication {
 
--- serviceribbon/src/main/java/com/zrain/cloud/ribbon/ServiceRibbonApplication.java
+++ serviceribbon/src/main/java/com/zrain/cloud/ribbon/ServiceRibbonApplication.java
@@ -2,15 +2,18 @@ package com.zrain.cloud.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.support.HttpAccessor;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ServiceRibbonApplication {
 
